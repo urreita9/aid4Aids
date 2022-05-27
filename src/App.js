17103387401dc06a08b0fbd4fc1c0919a3852b2e
urreita9 <francisco.urrea9@gmail.com>
2022-05-27 16:56:40 -0300

    React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Register } from './components/Register/Register';
+import { Login } from './components/Login/Login';
 
 function App() {
 	return (
 		<div className='App'>
-			<Register />
+			<BrowserRouter>
+				<Routes>
+					<Route path='/register' element={<Register />} />
+					<Route path='/login' element={<Login />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
