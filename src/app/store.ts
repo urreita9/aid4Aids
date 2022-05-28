c0aba56ae9e68fa80d1e3a5cd192147fe56a5e51
urreita9 <francisco.urrea9@gmail.com>
2022-05-28 13:10:58 -0300

    Slices added to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import productReducer from '../features/products/productsSlice';
 import cartReducer from '../features/cart/cartSlice';
+import userReducer from '../features/user/userSlice';
 
 export const store = configureStore({
-	reducer: { cart: cartReducer },
+	reducer: { cart: cartReducer, products: productReducer, user: userReducer },
 });
 
 export type AppDispatch = typeof store.dispatch;
