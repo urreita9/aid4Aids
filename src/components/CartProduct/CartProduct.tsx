95bc7e0135e77a102502881dce5e8da9824f2374
urreita9 <francisco.urrea9@gmail.com>
2022-05-29 05:40:11 -0300

    Add and substract product amount

--- src/components/CartProduct/CartProduct.tsx
+++ src/components/CartProduct/CartProduct.tsx
@@ -1,12 +1,23 @@
 import { CartProductContainer, CartProductImage } from './Styled';
 import { ProductColumn, ProductPrice, ProductTitle } from '../Product/Styled';
+import { RiCloseFill } from 'react-icons/ri';
+import { Button } from '../Button/Button';
+import { useAppDispatch } from '../../app/hooks';
+import { remove } from '../../features/cart/cartSlice';
 
 interface Props {
+	id: string;
 	imgURL: string;
 	price: number;
 	title: string;
+	amount: number;
 }
-export const CartProduct = ({ imgURL, title, price }: Props) => {
+export const CartProduct = ({ id, imgURL, title, price, amount }: Props) => {
+	const dispatch = useAppDispatch();
+
+	const deleteFromCart = () => {
+		dispatch(remove(id));
+	};
 	return (
 		<CartProductContainer>
 			<ProductColumn>
@@ -15,6 +26,8 @@ export const CartProduct = ({ imgURL, title, price }: Props) => {
 			<ProductColumn>
 				<ProductTitle>{title}</ProductTitle>
 				<ProductPrice>${price}</ProductPrice>
+				<ProductPrice>Quantity: {amount}</ProductPrice>
+				<Button onClick={deleteFromCart} text='Delete' />
 			</ProductColumn>
 		</CartProductContainer>
 	);
