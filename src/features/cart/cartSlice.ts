8217191ddef9bee0b371b421a13a5da962f4eb9a
urreita9 <francisco.urrea9@gmail.com>
2022-05-29 05:34:22 -0300

    + and - for each product.

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,12 +1,14 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface Product {
+	id: string;
 	isbn: number;
 	imgURL: string;
 	price: number;
 	title: string;
 	author: string;
 	publisher: string;
+	amount: number;
 }
 
 interface cartState {
@@ -23,18 +25,38 @@ const cartSlice = createSlice({
 	reducers: {
 		//Add
 		added(state, action: PayloadAction<Product>) {
-			state.products = [...state.products, action.payload];
-			localStorage.setItem('cart', JSON.stringify(state.products));
+			const copyOfState = [...state.products];
+
+			const exist = copyOfState.find(
+				(product) => product.id === action.payload.id
+			);
+			if (exist) {
+				state.products = copyOfState.map((product) => {
+					if (product.id === action.payload.id) {
+						product.amount += action.payload.amount;
+						return product;
+					} else {
+						return product;
+					}
+				});
+			} else {
+				state.products = [...state.products, action.payload];
+			}
 		},
 		//Remove
+		remove(state, action) {
+			const filtered = state.products.filter(
+				(product) => product.id !== action.payload
+			);
+			state.products = filtered;
+		},
 		//Empty
 		empty(state) {
 			state.products = [];
-			localStorage.setItem('cart', JSON.stringify(state.products));
 		},
 	},
 });
 
-export const { added, empty } = cartSlice.actions;
+export const { added, empty, remove } = cartSlice.actions;
 
 export default cartSlice.reducer;
