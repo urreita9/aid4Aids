23746fe6c9009008ab92a63da6bd9a7477f968f3
urreita9 <francisco.urrea9@gmail.com>
2022-05-29 01:12:05 -0300

    Login background image

---
+++ src/components/Login/Styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const LoginContainer = styled.div`
+	max-width: 400px;
+	height: 100vh;
+	background-color:#FFFFFF
+	border-radius: 5px;
+	margin: 0 auto;
+
+	@media (min-width: 380px) {
+		background-image: url(https://images-platform.99static.com//o5_Q8pUagynJuidDta7JRMJ7_K8=/311x281:1652x1622/fit-in/590x590/99designs-contests-attachments/74/74026/attachment_74026482);
+		background-repeat: no-repeat;
+		background-position: 25% 70%;
+		background-size: contain;
+	}
+`;
