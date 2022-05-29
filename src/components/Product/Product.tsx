9e16f092d8b454a3bcd2205a1a0370af41488620
urreita9 <francisco.urrea9@gmail.com>
2022-05-29 05:39:03 -0300

    Add and substract product amount

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -12,6 +12,7 @@ import {
 	ProductTitle,
 } from './Styled';
 import { Button } from '../Button/Button';
+import { useState } from 'react';
 
 interface Props {
 	_id: string;
@@ -24,19 +25,24 @@ interface Props {
 }
 
 export const Product = (props: Props) => {
+	const [amount, setAmount] = useState(1);
 	const dispatch = useAppDispatch();
-	const { title, imageURL, price, author, publisher, isbn } = props;
+	const { _id, title, imageURL, price, author, publisher, isbn } = props;
+
 	const handleClick = () => {
 		dispatch(
 			added({
+				id: _id,
 				title,
 				author,
 				price: +price,
 				imgURL: imageURL,
 				isbn: +isbn,
 				publisher,
+				amount,
 			})
 		);
+		setAmount(1);
 	};
 
 	return (
@@ -53,6 +59,21 @@ export const Product = (props: Props) => {
 					<ProductEditorial>{publisher}</ProductEditorial>
 				</ProductRow>
 				<ProductRow>
+					<Button
+						onClick={() => {
+							if (amount > 1) {
+								setAmount(amount - 1);
+							}
+						}}
+						text='-'
+					/>
+					{amount}
+					<Button
+						onClick={() => {
+							setAmount(amount + 1);
+						}}
+						text='+'
+					/>
 					<Button onClick={handleClick} text='Add to Cart' />
 				</ProductRow>
 			</ProductColumn>
