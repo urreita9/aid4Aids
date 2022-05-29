91d3f1087c12b472f515789e5dab56dd98eaaa6f
urreita9 <francisco.urrea9@gmail.com>
2022-05-29 05:32:55 -0300

    useForm error validations

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,22 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+export type Form = Record<string, string | number>;
+export type Errors = Record<string, string>;
+
+interface Props {
+	form: Form;
+	errors: Errors;
+	handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	handleSubmit: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
 export const useForm = (
-	initialState = {},
-	onSubmit: (arg: any) => void,
-	action: string
-) => {
+	initialState: Form,
+	onSubmit: (arg: Form) => void,
+	validate: (arg: Form) => Errors
+): Props => {
 	const [form, setForm] = useState(initialState);
-	const [errors, setErrors] = useState(initialState);
+	const [errors, setErrors] = useState({});
+	const [isSubmitting, setIsSubmitting] = useState(false);
+
+	useEffect(() => {
+		if (Object.keys(errors).length === 0 && isSubmitting) {
+			onSubmit(form);
+		}
+	}, [errors]);
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setForm({ ...form, [e.target.name]: e.target.value });
 	};
 
-	const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
 		e.preventDefault();
-
-		onSubmit(form);
+		setErrors(validate(form));
+		setIsSubmitting(true);
 	};
 
-	return { form, handleInputChange, handleSubmit };
+	return { form, errors, handleInputChange, handleSubmit };
 };
